1cc6743a994b29a25bdd5a8cc3eb48578eddd590
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-07-13 23:58:49 +0600

    13 july 2024

--- myapp.py
+++ myapp.py
@@ -1,4 +1,10 @@
+import pickle
 from flask import Flask, render_template, request
+
+
+with open('kmeans.pkl', 'rb') as file:
+    model = pickle.load(file)
+
 
 app = Flask(__name__)
 
@@ -12,7 +18,11 @@ def wtf():
         return render_template('tem.html')
     else:
         text1 = request.form['text1']
-        return render_template('tem.html', text2=text1)
+
+
+        prediction = model.predict([text1])  
+        return render_template('tem.html', text2=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
