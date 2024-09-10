3447c0672e3e1b158c99f3e28b86ea008339f898
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-09-10 15:20:01 +0600

    lll

--- myapp.py
+++ myapp.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 import joblib
-
 
 app = Flask(__name__)
 
-
+# Load models
 vectorizer = joblib.load('models/tfidf_vectorizer.pkl')
 pca_model = joblib.load('models/pca.pkl')
 umap_model = joblib.load('models/umap.pkl')
 dbscan_model = joblib.load('models/dbscan.pkl')
+kmeans_model = joblib.load('models/kmeans.pkl')
 
-
+# Preprocessing function
 def preprocess_text(text):
     cleaned_text = text
     vectorized_text = vectorizer.transform([cleaned_text])
@@ -18,16 +18,21 @@ def preprocess_text(text):
     umap_transformed = umap_model.transform(pca_transformed)
     return umap_transformed
 
-
+# Main route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         input_text = request.form['input_text']
         processed_data = preprocess_text(input_text)
-        prediction = dbscan_model.fit_predict(processed_data)
+        prediction = kmeans_model.predict(processed_data)
 
-        prediction_result = prediction
-        return render_template('result.html', prediction=prediction_result)
+        # Return JSON response
+        response = {
+            'index': str(prediction[0])  # Convert prediction to string if necessary
+        }
+        return jsonify(response)
+
+    # Render the HTML form
     return render_template('index.html')
 
 if __name__ == '__main__':
